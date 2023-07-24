add8f3a45f3cd2b6034494cd217b9c50c8e263e0
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-07-24 21:44:14 +0700

    Create Video Schema

---
+++ models/video.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const VideoSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    thumbnail_url: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Video', VideoSchema);
