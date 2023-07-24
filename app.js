99d272b6d17af0617d4fb8b59f47320209ca435d
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-07-24 21:39:34 +0700

    Set up database connection with Mongo DB

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const videosRouter = require('./routes/videos');
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+
+db.on('error', (error) => console.error(error));
+
+db.once('open', () => console.log('Connected to Database'));
 
 const app = express();
 
