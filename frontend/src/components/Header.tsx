85c514dab639b72b7a304f80753ee75ebc6a7b04
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-08-11 17:26:50 +0700

    Use Link from react-router

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,11 @@
+import {Link} from "react-router-dom";
+
 export default function Header() {
     return (
-        <nav className="py-6 px-4 border-b border-green-900 text-center">
-            <a href="/">Gigih Play</a>
+        <nav className="bg-primary text-black">
+            <div className="container p-4">
+                <Link to={'/'} className="font-medium text-xl">Gigih Play</Link>
+            </div>
         </nav>
     );
 }
