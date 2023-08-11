3cd244c4558a44054f526f94ec4dc4ae7a4a0535
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-08-12 01:10:21 +0700

    Update Header.tsx

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 export default function Header() {
     return (
         <nav className="bg-primary text-black">
-            <div className="container p-4">
+            <div className="container px-8 lg:px-0 py-4">
                 <Link to={'/'} className="font-medium text-xl">Gigih Play</Link>
             </div>
         </nav>
