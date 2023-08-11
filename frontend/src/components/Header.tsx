3ca8c3002fc3d5990f44445ac6d3ab1cf700ed68
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-08-11 16:22:44 +0700

    Add Header.tsx component

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <nav className="py-6 px-4 border-b border-green-900 text-center">
+            <a href="/">Gigih Play</a>
+        </nav>
+    );
+}
