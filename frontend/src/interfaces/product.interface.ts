3e8e0f3e82a8355a3f65fca6cc8eabb15f0356a7
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-08-12 13:04:36 +0700

    Add product.interface.ts

---
+++ frontend/src/interfaces/product.interface.ts
@@ -0,0 +1,10 @@
+interface ProductInterface{
+    _id: string,
+    videoId: string,
+    title: string,
+    price: number,
+    link: string,
+    photo: string
+}
+
+export default ProductInterface;
