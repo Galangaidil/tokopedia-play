14e02f8e6f8e76f2e1331efe76251291fb8d4379
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-08-12 15:30:02 +0700

    Add Loading.tsx component

---
+++ frontend/src/components/Loading.tsx
@@ -0,0 +1,9 @@
+function Loading() {
+    return (
+        <div className="min-h-screen grid place-content-center">
+            <span className="loading loading-infinity loading-lg"></span>
+        </div>
+    )
+}
+
+export default Loading;
