54a5e569b039db440e3d18c1e9ccbbf1bbd3a222
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-08-12 22:29:52 +0700

    Add skeletons

---
+++ frontend/src/components/VideoCardList.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import VideoInterface from "../interfaces/video.interface.ts";
+
+const VideoSkeleton: React.FC = () => {
+    return (
+        <div className="w-full bg-inherit rounded-lg overflow-hidden animate-pulse">
+            <div className="w-full h-[400px] bg-zinc-800"></div>
+            <p className="mt-6 h-6 bg-zinc-800"></p>
+        </div>
+    );
+};
+
+const VideoCard: React.FC<VideoInterface> = (props: VideoInterface) => {
+    const limit = (text: string, maxLength: number) => {
+        return text.length > maxLength ? text.substring(0, maxLength) + '...' : text;
+    }
+
+    return (
+        <div key={props._id}>
+            <Link to={`/videos/${props._id}`}>
+                <img
+                    src={props.thumbnail_url}
+                    alt={props.title}
+                    width={600}
+                    height={400}
+                    className="rounded-lg hover:rounded-none transition-all duration-500 ease-in-out w-full h-[400px] object-cover"
+                />
+                <p className={"mt-6"}>{limit(props.title, 30)}</p>
+            </Link>
+        </div>
+    )
+}
+
+interface VideoCardListProps {
+    videos: VideoInterface[];
+    isLoading: boolean;
+}
+
+const VideoCardList: React.FC<VideoCardListProps> = ({videos, isLoading}) => {
+    // Determine the number of skeletons to render
+    const numberOfSkeletons = 12; // For example, render 12 skeletons
+
+    // Render skeletons based on the determined number
+    const skeletonArray = Array.from({length: numberOfSkeletons});
+
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-6">
+            {isLoading
+                ? skeletonArray.map((_, index) => <VideoSkeleton key={index}/>)
+                : videos.map((video) => <VideoCard key={video._id} {...video} />)}
+        </div>
+    );
+}
+
+export default VideoCardList;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,24 +1,16 @@
 import VideoInterface from "../interfaces/video.interface.ts";
-import VideoCard from "../components/VideoCard.tsx";
 import useFetch from "../hooks/useFetch.ts";
-import Loading from "../components/Loading.tsx";
 import ErrorPage from "./500.tsx";
+import VideoCardList from "../components/VideoCardList.tsx";
 
 function Home() {
     const {data: videos, loading, error} = useFetch<VideoInterface[]>("http://127.0.0.1:3000/api/videos/");
-
-    if (loading) return <Loading/>;
 
     if (error) return <ErrorPage/>;
 
     return (
         <div className="container py-12 px-8 lg:px-0">
-            <div className="grid grid-cols-2 lg:grid-cols-6 gap-4 justify-items-stretch">
-                {videos?.map((video) => (
-                    <VideoCard key={video._id} {...video}/>
-                ))}
-            </div>
-
+            <VideoCardList videos={videos || []} isLoading={loading}/>
         </div>
     )
 }
