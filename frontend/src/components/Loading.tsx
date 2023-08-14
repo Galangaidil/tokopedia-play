5a87f0659036ff71cae400be43a12c400fb9136f
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-08-14 23:44:53 +0700

    Delete Loading.tsx

--- frontend/src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-function Loading() {
-    return (
-        <div className="min-h-screen grid place-content-center">
-            <span className="loading loading-infinity loading-lg"></span>
-        </div>
-    )
-}
-
-export default Loading;
