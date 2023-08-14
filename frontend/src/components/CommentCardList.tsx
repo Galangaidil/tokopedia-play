9c460023f651705ac31293a327d2e2ed9645c64e
Galang Aidil Akbar <73085251+Galangaidil@users.noreply.github.com>
2023-08-14 23:47:50 +0700

    remove bg on light mode

--- frontend/src/components/CommentCardList.tsx
+++ frontend/src/components/CommentCardList.tsx
@@ -4,9 +4,9 @@ import CommentInterface from "../interfaces/comment.interface.ts";
 const CommentSkeleton: React.FC = () => {
     return (
         <div role="status" className="w-full animate-pulse">
-            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
-            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
-            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+            <div className="h-2.5 rounded-full bg-gray-700 w-48 mb-4"></div>
+            <div className="h-2 rounded-full bg-gray-700 mb-2.5"></div>
+            <div className="h-2 rounded-full bg-gray-700 mb-2.5"></div>
         </div>
     );
 }
